8003191f8474f06369c88bab798c1b917dca2ca1
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-09 13:58:51 -0400

    created basic robot and basic function

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert('This is an alert! JavaScript is running!');
+
+
+var playerName = prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+console.log(playerName)
+function fight(){
+window.alert('The fight has begun!')
+}
+//fight();
